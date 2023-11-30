bde1e8a0a5fab08dc0457e9e3dade8b0cd64cf2d
jyjblrd <jyjbird@gmail.com>
2023-11-30 03:17:19 +0000

    bump version

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ URL = "https://github.com/lawrennd/fynesse_template"
 EMAIL = "emal@domain.here"
 AUTHOR = "Givenname Familyname"
 REQUIRES_PYTHON = ">=3.6.0"
-VERSION = "0.1.0"
+VERSION = "0.1.1"
 
 # What packages are required for this module to be executed?
 REQUIRED = [
