520be2ea30afd83f8943fa4ea4bf45ded3d94084
jyjblrd <jyjbird@gmail.com>
2023-12-01 04:01:55 +0000

    forgot abs

--- fynesse/assess.py
+++ fynesse/assess.py
@@ -239,7 +239,7 @@ def generate_neighbour_metrics_single_property(property, city_gdf, neighbour_rad
   price_difference_pct = np.abs(neighbours["price"] - property["price"]) / property["price"] if property["price"] is not None else None
   distance_to_neighbour = neighbours["geometry"].distance(property["geometry"])
   property_type_same = property["property_type"] == neighbours["property_type"]
-  date_delta = (property["date_of_transfer_unix_ns"] - neighbours["date_of_transfer_unix_ns"])
+  date_delta = np.abs(property["date_of_transfer_unix_ns"] - neighbours["date_of_transfer_unix_ns"])
 
   neighbour_metrics = pd.DataFrame({
       "price_difference_pct": price_difference_pct,
