769721ae480932de033eacf77d08035c0881f10b
jyjblrd <jyjbird@gmail.com>
2023-12-01 01:11:48 +0000

    style change

--- build/lib/fynesse/assess.py
+++ build/lib/fynesse/assess.py
@@ -29,7 +29,7 @@ import geopy.distance
 from geopy.geocoders import Nominatim
 from .helpers import *
 
-plt.style.use('seaborn-v0_8-pastel')
+plt.style.use('seaborn-v0_8-muted')
 
 
 """Place commands in this file to assess the data you have downloaded. How are missing values encoded, how are outliers encoded? What do columns represent, makes rure they are correctly labeled. How is the data indexed. Crete visualisation routines to assess the data (e.g. in bokeh). Ensure that date formats are correct and correctly timezoned."""
